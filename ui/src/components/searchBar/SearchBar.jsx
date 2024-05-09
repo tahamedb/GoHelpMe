c3d349f2688950497d9dd1bfe42fd17d09e6c59d
Yassamine <yassamineelm11@gmail.com>
2024-05-09 16:41:51 +0100

    search bar on welcome page now functional

--- ui/src/components/searchBar/SearchBar.jsx
+++ ui/src/components/searchBar/SearchBar.jsx
@@ -1,5 +1,6 @@
 import './searchBar.scss';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function SearchBar(){
 
@@ -7,16 +8,21 @@ function SearchBar(){
     city: "",
   });
   
+  const handleChange = (e) => {
+    setQuery((prev)=>({...prev, [e.target.name]: e.target.value}));
+  }
 
   return (
     <div className='searchBar'>
     <div className="type">
 </div>
     <form>
-      <input type = "text" name = "city" placeholder='City Location' />
+      <input type = "text" name = "city" placeholder='City Location' onChange={handleChange} />
+      <Link to={`/list?city=${query.city}`}>
       <button >
         <img src="/search.png" alt="" />
       </button>
+      </Link>
     </form>
     </div>
   )
