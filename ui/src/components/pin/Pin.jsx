6d40169cff264ef940bd32858041b9f249b31c39
yasminnelm <yassamineelm11@gmail.com>
2024-06-09 13:48:57 +0100

    css fix

--- ui/src/components/pin/Pin.jsx
+++ ui/src/components/pin/Pin.jsx
@@ -1,22 +1,26 @@
-import { Marker, Popup } from 'react-leaflet'
-import './pin.scss'
-import { Link } from 'react-router-dom'
+import { Marker, Popup } from "react-leaflet";
+import "./pin.scss";
+import { Link } from "react-router-dom";
 
-function Pin({item}){
+function Pin({ item }) {
   return (
-    <Marker position={[item.latitude , item.longitude]}>
+    <Marker position={[item.latitude, item.longitude]}>
       <Popup>
         <div className="popupContainer">
-            <img src={item.img} alt="" />
-            <div className="textContainer">
-                <Link to={`/${item.id}`}>{item.title}</Link>
-                <progress value={item.participants} max={item.totalParticipants} className="progressBar" ></progress>
-                <span>{item.participants} participants</span>
-            </div>
+          <img src={item.images[0]} alt="" />
+          <div className="textContainer">
+            <Link to={`/${item.id}`}>{item.title}</Link>
+            <progress
+              value={item.participants}
+              max={item.totalParticipants}
+              className="progressBar"
+            ></progress>
+            <span>{item.participants} participants</span>
+          </div>
         </div>
       </Popup>
     </Marker>
-  )
+  );
 }
 
-export default Pin
+export default Pin;
